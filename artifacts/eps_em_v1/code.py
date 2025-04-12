2fddd6434c214446a90bb8eadcbdef336219001c
Randall Scharpf <randall.scharpf@gmail.com>
2025-04-11 20:22:37 -0700

    add submodule to undisclosable code

--- artifacts/eps_em_v1/code.py
+++ artifacts/eps_em_v1/code.py
@@ -28,29 +28,33 @@ async def gathered_task():
 
 
 if __name__ == "__main__":
+    # TODO: uncomment to run flight code (only once the electrical parts of the PCB are validated)
     # asyncio.run(gathered_task())
+
     import board
     import digitalio
+    # TODO: uncomment pins to run electrical tests and validate the PCB
     for BMS_SHD in [
-        board.S1_CHARGE_SHD,
-        board.S1_DISCHARGE_SHD,
-        board.S2_CHARGE_SHD,
-        board.S2_DISCHARGE_SHD,
-        board.S3_CHARGE_SHD,
-        board.S3_DISCHARGE_SHD,
+        # board.S1_CHARGE_SHD,
+        # board.S1_DISCHARGE_SHD,
+        # board.S2_CHARGE_SHD,
+        # board.S2_DISCHARGE_SHD,
+        # board.S3_CHARGE_SHD,
+        # board.S3_DISCHARGE_SHD,
     ]:
         pin = digitalio.DigitalInOut(BMS_SHD)
         pin.direction = digitalio.Direction.OUTPUT
         pin.value = False
     for EN_PSU in [
-        board.EN_3V3_BUS,
-        board.EN_5V_BUS,
+        # board.EN_3V3_BUS,
+        # board.EN_5V_BUS,
         # board.EN_12VLP_BUS,
-        board.EN_12VHP_BUS,
-        board.MPPT_EN
+        # board.EN_12VHP_BUS,
+        # board.MPPT_EN
     ]:
         pin = digitalio.DigitalInOut(EN_PSU)
         pin.direction = digitalio.Direction.OUTPUT
         pin.value = True
+    nda_libraries.run_if_nda_libraries_available(run_secret_functions)
     while True:
         pass
