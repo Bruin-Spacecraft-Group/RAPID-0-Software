751003d6ed9dffebdfad7b6740b613d916a07e56
Connor Perrone <connorperrone@gmail.com>
2025-05-21 12:04:56 -0700

    Create CDH EM board artifact

---
+++ artifacts/cdh_em_board/board.py
@@ -0,0 +1,13 @@
+"""
+Pin definitions of the CDH engineering model board.
+
+The variables defined in this file should be used when working with the corresponding pins
+instead of the previous approach of importing the CircuitPython defined 'board' and
+'microcontroller' module(s). This ensures any future changes to the board can be
+easily integrated into the existing codebase.
+"""
+
+import microcontroller
+
+# TODO: Define pins here
+#PIN_NAME_HERE = microcontroller.pin...
---
+++ artifacts/cdh_em_board/code.py
@@ -0,0 +1,12 @@
+import asyncio
+
+async def gathered_task():
+    """Runs all tasks in parallel."""
+    await asyncio.gather(
+
+    )
+
+
+if __name__ == "__main__":
+    # asyncio.run(gathered_task())
+    print('CDH code.py has been run successfully.')
