6a03ef89b806e3e350ec98e67cd4fe1a6c633ac5
Connor Perrone <connorperrone@gmail.com>
2025-05-21 21:11:41 -0700

    Define pins in board.py for CDH EM board

--- artifacts/cdh_em_board/board.py
+++ artifacts/cdh_em_board/board.py
@@ -9,5 +9,30 @@ easily integrated into the existing codebase.
 
 import microcontroller
 
-# TODO: Define pins here
-#PIN_NAME_HERE = microcontroller.pin...
+
+# Pins which connect to the camera
+CAM_HEAT_N = microcontroller.pin.PB7
+CAM_SCL = microcontroller.pin.PB8
+CAM_SDA = microcontroller.pin.PB9
+CAM_SCK = microcontroller.pin.PE2
+CAM_CS = microcontroller.pin.PE3
+CAM_MISO = microcontroller.pin.PE5
+CAM_MOSI = microcontroller.pin.PE6
+
+
+# RS485 pins for inter-subsystem communication
+RS485_1_RX = microcontroller.pin.PE7
+RS485_1_TX = microcontroller.pin.PE8
+RS485_1_DE = microcontroller.pin.PE9
+
+RS485_2_RX = microcontroller.pin.PB12
+RS485_2_TX = microcontroller.pin.PB13
+RS485_2_DE = microcontroller.pin.PB14
+
+RS485_3_RX = microcontroller.pin.PE0
+RS485_3_TX = microcontroller.pin.PE1
+RS485_3_DE = microcontroller.pin.PD15
+
+
+# Watchdog pin
+WDI = microcontroller.pin.PC13
