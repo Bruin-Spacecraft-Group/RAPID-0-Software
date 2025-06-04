7293f928128746efd56d762ef2c55001026bb30b
AstroPancake72 <owen.t.rusk@gmail.com>
2025-06-03 19:01:24 -0700

    cdh_em_board board.py definitions update

--- artifacts/cdh_em_board/board.py
+++ artifacts/cdh_em_board/board.py
@@ -11,26 +11,26 @@ import microcontroller
 
 
 # Pins which connect to the camera
-CAM_HEAT_N = microcontroller.pin.PB7
-CAM_SCL = microcontroller.pin.PB8
-CAM_SDA = microcontroller.pin.PB9
-CAM_SCK = microcontroller.pin.PE2
-CAM_CS = microcontroller.pin.PE3
-CAM_MISO = microcontroller.pin.PE5
-CAM_MOSI = microcontroller.pin.PE6
+CAM_HEAT_N = microcontroller.pin.PB07
+CAM_SCL = microcontroller.pin.PB08
+CAM_SDA = microcontroller.pin.PB09
+CAM_SCK = microcontroller.pin.PE02
+CAM_CS = microcontroller.pin.PE03
+CAM_MISO = microcontroller.pin.PE05
+CAM_MOSI = microcontroller.pin.PE06
 
 
 # RS485 pins for inter-subsystem communication
-RS485_1_RX = microcontroller.pin.PE7
-RS485_1_TX = microcontroller.pin.PE8
-RS485_1_DE = microcontroller.pin.PE9
+RS485_1_RX = microcontroller.pin.PE07
+RS485_1_TX = microcontroller.pin.PE08
+RS485_1_DE = microcontroller.pin.PE09
 
 RS485_2_RX = microcontroller.pin.PB12
 RS485_2_TX = microcontroller.pin.PB13
 RS485_2_DE = microcontroller.pin.PB14
 
 RS485_3_RX = microcontroller.pin.PE0
-RS485_3_TX = microcontroller.pin.PE1
+RS485_3_TX = microcontroller.pin.PE01
 RS485_3_DE = microcontroller.pin.PD15
 
 
@@ -39,7 +39,7 @@ WDI = microcontroller.pin.PC13
 
 
 # Battery Pack Heater Pins
-BAT_HEAT1N = microcontroller.pin.PA1
+BAT_HEAT1N = microcontroller.pin.PA01
 BAT_HEAT2N = microcontroller.pin.PA0
 
 # USB Communication Pins
