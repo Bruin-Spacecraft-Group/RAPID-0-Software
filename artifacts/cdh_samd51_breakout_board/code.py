0e8f92f6787ed889e6080ddb39424e9072d5576f
AstroPancake72 <owen.t.rusk@gmail.com>
2025-06-06 19:54:39 -0700

    added samd51 breakout board artifact

---
+++ artifacts/cdh_samd51_breakout_board/code.py
@@ -0,0 +1,12 @@
+import asyncio
+from tasks import inter_subsystem_rs485
+
+async def gathered_task():
+    await asyncio.gather(
+        inter_subsystem_rs485.cdh_em_board_rs485_reciever_task()
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(gathered_task())
+    print('CDH code.py has been run successfully.')
