2ab637f63880b7a4a0ee53ac9c602a166cbde203
AstroPancake72 <owen.t.rusk@gmail.com>
2025-08-04 22:25:58 -0700

    Added final steps to finish first draft

--- src/tasks/mekf.py
+++ src/tasks/mekf.py
@@ -19,7 +19,8 @@ def mekf_update(
     Q_noise: np.ndarray,
     v_body: np.ndarray,
     v_inertial: np.ndarray,
-    dt: float,
+    R_meas: np.ndarray,
+    dt: float
 ) -> Quaternion:
     """
     Calculates the attitude using a Multiplicative Extended Kalman Filter (MEKF) based on:
@@ -30,9 +31,10 @@ def mekf_update(
                             the less error resulting from the MEKF. Assumed starting basis for which w_ref acts on
         w_ref (np.ndarray): Angular acceleration vector in the body reference
         P (np.ndarray): 3x3 Covariance matrix (ensure this isn't the 0 matrix when starting or the filter won't update effectively)
-        Q_noise: 3x3 Diagonal matrix representing noise/bias from the gyro sensor
+        Q_noise: 3x3 Diagonal matrix representing noise/bias from the gyro sensor (given as sigma^2 * I_3)
         v_body: Measured vector from body frame (from magnetometer or sun sensor)
         v_inertial: Expected vector measurement at Quaternion(1,0,0,0) orientation
+        R_meas: 3x3 Diagonal matrix representing noise/bias from the measured body vector (given as sigma^2 * I_3)
         dt (float): Amount of time that has passed since a new estimate for q_ref has been made (in ms?)
 
     Returns:
@@ -42,14 +44,14 @@ def mekf_update(
     # Step 1: Calculate expected quaternion from kinematics
     # Equation (9) from Markley paper
     omega_q = Quaternion(0.0, *w_ref, normalize=False)
-    q = (q_ref + (0.5 * dt * (omega_q * q_ref))).normalize()
+    q_ref = (q_ref + (0.5 * dt * (omega_q * q_ref)))
 
     # Step 2: Calculate covariance matrix
     # Equation (14) from Markley paper
     F_a = skew(w_ref)
     G = np.eye(3)
     P = (
-        P + (np.dot(F, P) + np.dot(P, -F) + np.dot(G, np.dot(Q_noise, G))) * dt
+        P + (np.dot(F_a, P) + np.dot(P, -F_a) + np.dot(G, np.dot(Q_noise, G))) * dt
     )  # If G is just identity matrix, can reduce dot operations
 
     # Step 3: Predict measured vector
@@ -60,4 +62,24 @@ def mekf_update(
     # Equation (19) from Markley paper
     H = skew(v_pred)
 
-    return q
+    # TODO: Double check this part and following parts are right
+    # Step 5: Kalman Gain
+    # Equation (22) from Markley paper(?)
+    S = np.dot(H, np.dot(P, -H)) + R_meas
+    K = np.dot(P, np.dot(-H, np.linalg.inv(S))) # Calculating the inverse matrix can result in singularities, should check
+
+    # Step 6: Delta_a calculation
+    innovation = v_body - v_pred
+    delta_a = np.dot(K, innovation)  # 3x1 correction vector a
+
+    # Step 7: Covariance update
+    I = np.eye(3)
+    P = np.dot(I - np.dot(K, H), P)
+
+    # Step 8: Apply attitude correction using small-angle approximation
+    # Equation (6) and (7) from Markley paper
+    dq = Quaternion(1.0, *(0.5 * delta_a))
+    q_ref = dq * q_ref
+    q_ref.normalize()
+
+    return q_ref
