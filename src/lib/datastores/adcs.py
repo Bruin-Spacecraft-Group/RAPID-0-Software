0f82be04533856fdbb215e79c6ec7e25d19ba3c5
AstroPancake72 <owen.t.rusk@gmail.com>
2025-10-18 20:39:36 -0700

    Added ADCS datastore

---
+++ src/lib/datastores/adcs.py
@@ -0,0 +1,5 @@
+"""
+Module of objects and classes which store data pertaining to the current state of the
+EPS system for all tasks to update and use. Readings that have not yet been initialized
+are set to `None` throughout this module.
+"""
