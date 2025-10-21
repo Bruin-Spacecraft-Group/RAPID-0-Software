d9eef5a94867e698b9fcf4b297da201f34f630e2
TinFoiKa <aaronli69@outlook.com>
2025-10-21 00:29:20 -0700

    Updated comments, Appeased static analyser

--- src/tasks/adcs/nominal.py
+++ src/tasks/adcs/nominal.py
@@ -12,11 +12,10 @@ def get_current_time():
     Placeholder function for getting the current time on satellite
     """
 
-    pass
-
-def get_sensor_data():
+def get_sensor_data() -> tuple[float, ...]:
     """
-    Polls sun sensor, magnetometer, and model data for sun and geomagnetic data.
+    Polls sun sensor, magnetometer, and model data for sun and geomagnetic data;
+    Polls gyro for angular acceleration
     Returns a tuple of 5 floats with the organisation:
     [sun_sensor, sun_model, magnetometer, mag_model, gyro_alpha]
     """
@@ -30,14 +29,15 @@ def nominal_tasks(datastore: Datastore):
 
     datastore.current_time = get_current_time()
     
-    if (datastore.current_time - datastore.last_cdh_update > datastore.update_interval):
+    if datastore.current_time - datastore.last_cdh_update > datastore.update_interval:
         update_attitude(datastore)
 
         datastore.last_cdh_update = datastore.current_time
 
 def update_attitude(datastore: Datastore):
     """
-    Using data from sensors and TRIAD algorithm, calculate, update, and process datastore attitude
+    Using data from sensors and TRIAD algorithm
+    calculates and processes attitude to update datastore values
     """
 
     # Placeholders for sun sensor data, sun model data, magnetometer data, and geomagnetic model data
@@ -49,19 +49,18 @@ def update_attitude(datastore: Datastore):
     p_data = s_data
 
     # if sun sensor is unavailable, transition to magnetometer, keep using old quaternion
-    if (s_data == None):
+    if s_data is None:
         p_data = mag_data
 
         # no quaternion update (?) TODO for operations?
         datastore.quaternion = datastore.quaternion
-    else: 
+    else:
         # IFF sun sensor can be used, use TRIAD algorithm to determine attitude from sensor + model data
         [triad_q, msg] = triad_algorithm(s_model, mag_model, s_data, mag_data)
 
         match msg:
             case 0: # success
                 datastore.quaternion = triad_q
-                pass
             case 1: # anti-parallel
                 pass
             case 2: # FAIL : Colinear
@@ -74,11 +73,12 @@ def update_attitude(datastore: Datastore):
                 pass
 
     # Clean, update data with MEKF
-    datastore.quaternion = mekf_update(datastore.quaternion, 
-                                       alpha, 
-                                       datastore.cv_matrix, 
-                                       datastore.n_matrix, 
-                                       p_data, 
-                                       datastore.v_inertial, 
-                                       datastore.r_meas, 
-                                       datastore.dt) 
+    datastore.quaternion = mekf_update(
+        datastore.quaternion, 
+        alpha, 
+        datastore.cv_matrix, 
+        datastore.Q_noise, 
+        p_data, 
+        datastore.v_inertial, 
+        datastore.r_meas, 
+        datastore.dt)
