354c0015bc9ec9fddfee825d70a3a04c2b3db531
Megh Joshi <megh07joshi@gmail.com>
2025-10-24 17:03:08 -0700

    added adcs datastore values

--- src/lib/datastores/adcs.py
+++ src/lib/datastores/adcs.py
@@ -4,9 +4,14 @@ ADCS system for all tasks to update and use. Readings that have not yet been ini
 are set to `None` throughout this module.
 """
 
+
 class Datastore:
-    """
-    """
+    """ """
 
     def __init__(self):
         self.current_time = None
+        self.last_cdh_update = None
+        self.update_interval = 1.0  # secondsgit
+        self.quaternion = (
+            None  # Quaternion representing attitude from body frame to inertial frame
+        )
