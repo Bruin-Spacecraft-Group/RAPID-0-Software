ba4dca370421ba519169e9fdeaff4946fc80f627
TinFoiKa <aaronli69@outlook.com>
2025-10-26 14:02:33 -0700

    Updated include.json and fixed minor code + static analyser errors

--- src/tasks/adcs/detumble.py
+++ src/tasks/adcs/detumble.py
@@ -1,5 +1,13 @@
+"""
+Module for handling the Detumble procedure for ADCS
+"""
+
 from datastores.adcs import Datastore
 
 
-def detumble():
-    print("Detumble mode. ")
+def detumble(datastore: Datastore):
+    """
+    Empty function declaration to be used by loop.py in larger ADCS loop
+    """
+
+    print(datastore.mode + 'Mode')
