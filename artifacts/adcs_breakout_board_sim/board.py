28a2dad3eee880a4ba83f09a831d0249439d7b8f
ishyabahl <ishyabahl@gmail.com>
2025-11-10 20:38:32 -0800

    git commit -m "Updated pins for adcs board"

--- artifacts/adcs_breakout_board_sim/board.py
+++ artifacts/adcs_breakout_board_sim/board.py
@@ -1,3 +1,13 @@
 """
 Pin definitions for the board which runs the adcs_breakout_board_sim artifact.
 """
+
+import microcontroller
+
+#IMU (BMI088) Bus Pins
+GYRO_SCLK = microcontroller.pin.PA5
+GYRO_MOSI = microcontroller.pin.PA7
+GYRO_MISO = microcontroller.pin.PA6
+GYRO_CS1 = microcontroller.pin.PA10
+GYRO_CS2 = microcontroller.pin.PA11
+GYRO_CS3 = microcontroller.pin.PA12
