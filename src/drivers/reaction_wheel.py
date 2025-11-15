e6c6ccd5abe8a0b92e99239a58566446700aab5f
TinFoiKa <aaronli69@outlook.com>
2025-11-14 16:14:54 -0800

    Doc strings, final improvements on outward facing reactionwheel functions

--- src/drivers/reaction_wheel.py
+++ src/drivers/reaction_wheel.py
@@ -20,27 +20,35 @@ class ReactionWheel:
         self.fg = digitalio.DigitalInOut(fg)
         self.fg.direction = digitalio.Direction.INPUT
         
-    def get_duty_cycle(self):
+    def get_speed(self):
         """
-        Returns the duty cycle value of the motor pwm
+        Returns the duty cycle value of the motor (-2^16 to 2^16)
+        
+        Returns Positive if Clockwise, Negative if Counterclockwise
         """
-        return self.unsoll.duty_cycle
+        
+        dire = 1 if self.get_direction() else -1
+
+        return dire * self.unsoll.duty_cycle
     
     def get_direction(self):
         """
         Returns motor direction
-        True if Clockwise, False if Anticlockwise
+
+        Returns True if Clockwise, False if Anticlockwise
         """
         return self.diro.value
     
-    def set_duty_cycle(self, dc):
+    def set_speed(self, dc):
         """
-        Sets duty cycle and direction (velocity)
+        Sets duty cycle and direction (much like velocity)
         Positive dc values are clockwise and Negative values anticlockwise
+        
+        Accepts inputs dc from domain [-2^16 to 2^16]
         """
         if dc >= 0:
             self.diro.value = True # Clockwise
-            self.unsoll.duty_cycle = dc
+            self.unsoll.duty_cycle = dc 
         elif dc < 0:
             self.diro.value = False # counterclockwise
             self.unsoll.duty_cycle = -dc
