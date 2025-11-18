833ca4cf14b4669708d13d95e64b9c601b7a2d81
chenwen051116 <TavenChen@outlook.com>
2025-11-17 20:05:45 -0800

    Update pin names and asyncio imports for board and driver
    Corrected pin names in board.py to match microcontroller convention. Updated asyncio imports in code.py and bmi088.py to use Adafruit_CircuitPython_asyncio. Refactored code.py to add a main() function and fixed asyncio.run usage. Adjusted include.json submodules and file lists for consistency.

--- artifacts/adcs_breakout_board_sim/board.py
+++ artifacts/adcs_breakout_board_sim/board.py
@@ -5,9 +5,9 @@ Pin definitions for the board which runs the adcs_breakout_board_sim artifact.
 import microcontroller
 
 #IMU (BMI088) Bus Pins
-GYRO_SCLK = microcontroller.pin.PA5
-GYRO_MOSI = microcontroller.pin.PA7
-GYRO_MISO = microcontroller.pin.PA6
+GYRO_SCLK = microcontroller.pin.PA05
+GYRO_MOSI = microcontroller.pin.PA07
+GYRO_MISO = microcontroller.pin.PA06
 GYRO_CS1 = microcontroller.pin.PA10
 GYRO_CS2 = microcontroller.pin.PA11
 GYRO_CS3 = microcontroller.pin.PA12
