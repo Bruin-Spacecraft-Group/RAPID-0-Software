2bb474c1602a8bc2b44ada1507762cfb21498c62
Andrew <andrewpai736@gmail.com>
2025-11-21 17:39:16 -0800

    Updated read and write methods

--- src/drivers/mmc5983ma.py
+++ src/drivers/mmc5983ma.py
@@ -10,7 +10,7 @@ from adafruit_bus_device.spi_device import SPIDevice
 
 
 class MMC5983MA:
-    def __init__(self, sck_pin, mosi_pin, miso_pin, ss_pin, baudrate=1000000):
+    def __init__(self, sck_pin, mosi_pin, miso_pin, ss_pin, _baudrate=1000000, _polarity = 0, _phase = 0):
         try:
             self.spi = busio.SPI(sck_pin, MOSI=mosi_pin, MISO=miso_pin)
         except ValueError as e:
@@ -18,6 +18,9 @@ class MMC5983MA:
                 "Failed to Initialize SPI. Check pin assignments."
             ) from e
         pm = pin_manager.PinManager.get_instance()
+        self.baudrate = _baudrate
+        self.polarity = _polarity
+        self.phase = _phase
         self.ss_pin = pm.create_digital_in_out(ss_pin)
         self.ss_pin.direction = digitalio.Direction.OUTPUT
         self.ss_pin.value = True
@@ -27,4 +30,25 @@ class MMC5983MA:
 
     def read_register(self, register_address, length=1):
         with self.spi_device as spi:
-            spi.write()
+            while not self.spi.try_lock():
+                pass
+            try:
+                self.spi.configure(self.baudrate, self.polarity, self.phase)
+                self.ss_pin.value = False
+                self.spi.read(register_address)
+                self.ss_pin.value = True
+            finally:
+                self.spi.unlock()
+
+
+    def write_register(self, register_address):
+        with self.spi as spi:
+            while not self.spi.try_lock():
+                pass
+            try:
+                self.spi.configure(self.baudrate, self.polarity, self.phase)
+                self.ss_pin.value = False
+                self.spi.write(register_address)
+                self.ss_pin.value = True
+            finally:
+                self.spi.unlock()
