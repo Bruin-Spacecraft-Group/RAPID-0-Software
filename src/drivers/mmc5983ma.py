85ad42ea86093eb68b0e5f29465c9ba6d2159442
Andrew <andrewpai736@gmail.com>
2025-11-24 20:44:15 -0800

    Adding read+write interaction and removing SPIDevice from code

--- src/drivers/mmc5983ma.py
+++ src/drivers/mmc5983ma.py
@@ -8,7 +8,6 @@ import digitalio
 import busio
 from adafruit_bus_device.spi_device import SPIDevice
 
-
 class MMC5983MA:
     def __init__(self, sck_pin, mosi_pin, miso_pin, ss_pin, _baudrate=1000000, _polarity = 0, _phase = 0):
         try:
@@ -24,25 +23,34 @@ class MMC5983MA:
         self.ss_pin = pm.create_digital_in_out(ss_pin)
         self.ss_pin.direction = digitalio.Direction.OUTPUT
         self.ss_pin.value = True
-        self.spi_device = SPIDevice(
-            self.spi, self.ss_pin, baudrate=baudrate, polarity=0, phase=0
-        )
 
+# ---- SPI Interactions W/O SPIDevice ----
+    # ---- Read and Write Interaction ----
+    def read_and_write_register(self, read_register_address, write_register_address):
+        while not self.spi.try_lock():
+            pass
+        try:
+            self.spi.configure(self.baudrate, self.polarity, self.phase)
+            self.ss_pin.value = False
+            self.spi.write_readinto(read_register_address, write_register_address)
+            self.ss_pin.value = True
+        finally:
+            self.spi.unlock()
+
+    # ---- Read Only Interaction ----
     def read_register(self, register_address, length=1):
-        with self.spi_device as spi:
-            while not self.spi.try_lock():
-                pass
-            try:
-                self.spi.configure(self.baudrate, self.polarity, self.phase)
-                self.ss_pin.value = False
-                self.spi.read(register_address)
-                self.ss_pin.value = True
-            finally:
-                self.spi.unlock()
+        while not self.spi.try_lock():
+            pass
+        try:
+            self.spi.configure(self.baudrate, self.polarity, self.phase)
+            self.ss_pin.value = False
+            self.spi.readinto(register_address)
+            self.ss_pin.value = True
+        finally:
+            self.spi.unlock()
 
-
+    # ---- Write Only Interaction ----
     def write_register(self, register_address):
-        with self.spi as spi:
             while not self.spi.try_lock():
                 pass
             try:
