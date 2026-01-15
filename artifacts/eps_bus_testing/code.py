d28d6f27d53a50aab839c54b7947e11f0d28bb5f
Connor Perrone <connorperrone@gmail.com>
2026-01-14 18:09:49 -0800

    Create function headers for EPS bus testing

--- artifacts/eps_bus_testing/code.py
+++ artifacts/eps_bus_testing/code.py
@@ -2,5 +2,24 @@
 Allows for enabling/disabling each of the buses on the EPS board.
 """
 
+from datastores.eps import DsCommands
+import board
+
 if __name__ == "__main__":
+    pass
+
+
+def set_3v3(eps_ds_commands: DsCommands, enable: bool):
+    pass
+
+
+def set_5v(eps_ds_commands: DsCommands, enable: bool):
+    pass
+
+
+def set_12vlp(eps_ds_commands: DsCommands, enable: bool):
+    pass
+
+
+def set_12vhp(eps_ds_commands: DsCommands, enable: bool):
     pass
