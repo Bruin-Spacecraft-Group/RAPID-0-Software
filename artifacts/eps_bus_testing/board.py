c0dfe936f5d84c58b1680f22cdd07ccd3e6bc8d6
Connor Perrone <connorperrone@gmail.com>
2026-01-14 18:07:23 -0800

    Add pin definitions for EPS bus testing artifact

---
+++ artifacts/eps_bus_testing/board.py
@@ -0,0 +1,6 @@
+import microcontroller
+
+EN_3V3_BUS = microcontroller.pin.PA14
+EN_5V_BUS = microcontroller.pin.PA15
+EN_12VLP_BUS = microcontroller.pin.PD03
+EN_12VHP_BUS = microcontroller.pin.PD00
