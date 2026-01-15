daac5dffa0bef8ee25582d399db2bc872120c0ab
Bobydong <boby9er@gmail.com>
2026-01-14 17:51:25 -0800

    init code

---
+++ src/tasks/adcs/reaction_wheel_PD.py
@@ -0,0 +1,12 @@
+KP = 1
+KD = 1
+
+prev_speed = 0
+current_speed = 0
+
+
+def reaction_wheel_pd_control():
+    current_speed = get_speed() # temp
+    prev_speed = get_prev_speed() # temp
+
+    KD * 
