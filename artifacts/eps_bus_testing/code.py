5c52d0a6d5a34562228eb63f5a78c700732ccd85
Connor Perrone <connorperrone@gmail.com>
2026-01-14 18:53:07 -0800

    Implement calls to EPS bus testing from commands

--- artifacts/eps_bus_testing/code.py
+++ artifacts/eps_bus_testing/code.py
@@ -7,7 +7,48 @@ import board
 import digitalio
 
 if __name__ == "__main__":
-    pass
+    print("=" * 100)
+    print(f'{"=" * 40}  EPS Bus Testing  {"=" * 41}')
+    print("=" * 100)
+
+    ds_commands = DsCommands()
+
+    while True:
+        print()
+        command = input(
+            "Enter a command to control a bus (syntax: [enable/disable] [3v, 5v, 12vlp, 12vhp]):"
+        )
+
+        split = command.split()
+
+        if split.__len__ != 2:
+            print(
+                f"Invalid syntax. There should be 2 arguments separated by a space but you input {split.__len__}."
+            )
+            continue
+
+        if split[0] == "enable":
+            enable = True
+        elif split[0] == "disable":
+            enable = False
+        else:
+            print(
+                f'Invalid syntax. The argument "{split[0]}" must be either "enable" or "disable".'
+            )
+            continue
+
+        if split[1] == "3v3":
+            set_3v3(ds_commands, enable)
+        elif split[1] == "5v":
+            set_5v(ds_commands, enable)
+        elif split[1] == "12vlp":
+            set_12vlp(ds_commands, enable)
+        elif split[1] == "12vhp":
+            set_12vhp(ds_commands, enable)
+        else:
+            print(
+                f'Invalid syntax. The argument "{split[1]}" must be either "3v3", "5v", "12vlp", or "12vhp".'
+            )
 
 
 def set_3v3(eps_ds_commands: DsCommands, enable: bool):
