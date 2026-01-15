736d42ff8ee0926ec99e330b399a5686def6ebee
Connor Perrone <connorperrone@gmail.com>
2026-01-14 18:25:23 -0800

    Define 12V low power and 12V high power bus testing functions

--- artifacts/eps_bus_testing/code.py
+++ artifacts/eps_bus_testing/code.py
@@ -4,6 +4,7 @@ Allows for enabling/disabling each of the buses on the EPS board.
 
 from datastores.eps import DsCommands
 import board
+import digitalio
 
 if __name__ == "__main__":
     pass
@@ -18,8 +19,14 @@ def set_5v(eps_ds_commands: DsCommands, enable: bool):
 
 
 def set_12vlp(eps_ds_commands: DsCommands, enable: bool):
-    pass
+    en_12vlp_bus_pin = digitalio.DigitalInOut(board.EN_12VLP_BUS)
+    en_12vlp_bus_pin.direction = digitalio.Direction.OUTPUT
+    en_12vlp_bus_pin.value = enable
+    eps_ds_commands.bus_12vlp_enabled = enable
 
 
 def set_12vhp(eps_ds_commands: DsCommands, enable: bool):
-    pass
+    en_12vhp_bus_pin = digitalio.DigitalInOut(board.EN_12VHP_BUS)
+    en_12vhp_bus_pin.direction = digitalio.Direction.OUTPUT
+    en_12vhp_bus_pin.value = enable
+    eps_ds_commands.bus_12vhp_enabled = enable
