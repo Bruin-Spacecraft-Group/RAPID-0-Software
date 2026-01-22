75bb135a6b307fc445b3e61e23c3a6e01c109203
SuperCoder109 <garagshashank7@gmail.com>
2026-01-21 17:47:23 -0800

    Fixed PD Loop logic, used proper time library, added comments/improved clarity

--- src/tasks/adcs/reaction_wheel_PD.py
+++ src/tasks/adcs/reaction_wheel_PD.py
@@ -2,11 +2,12 @@
 Reaction wheel PD controller loop.
 """
 
-import submodules.Adafruit_CircuitPython_Ticks.adafruit_ticks as timer
+import time
 
 KP = 1
 KD = 1
 ERROR_MARGIN = 0
+DELAY = 10
 
 prev_speed = 0
 current_speed = 0
@@ -14,19 +15,26 @@ current_speed = 0
 
 def reaction_wheel_pd_control(desired_speed, motor):
 
-    prev_speed = motor.get_speed()  # temp
-    start_time = timer.ticks_ms()
+    # Retrieve the initial speed of the motor and start the initial timer
+    prev_speed = motor.get_speed()
+    prev_time = time.monotonic_ns()
 
+    # Set the current speed to the initial speed of the motor
     current_speed = prev_speed
 
+    # While there is a significant error between the current and desired speed
     while abs(desired_speed - current_speed) > ERROR_MARGIN:
-        current_speed = motor.get_speed()  # temp
-        end_time = timer.ticks_ms()
+
+        # Start the timer for the beginning of the PD loop and calculate P and D terms
+        current_time = time.monotonic_ns()
         p_term = current_speed * KP
-        d_term = (
-            KD * (current_speed - prev_speed) / timer.ticks_diff(end_time, start_time)
-        )
+        d_term = KD * (current_speed - prev_speed) / (prev_time - current_time)
         prev_speed = current_speed
-        start_time = timer.ticks_ms()
+
+        # Reset the initial timer (end of PD loop) and apply changed values to motor speed
+        prev_time = time.monotonic_ns()
         motor.set_speed(p_term + d_term)
+
+        # Retrieve the new speed of the motor and implement loop delay
         current_speed = motor.get_speed()
+        time.sleep(10)
