13713ff2d5acb88fbe9b17575f470ccc536e81c9
Connor Perrone <connorperrone@gmail.com>
2026-01-27 20:35:30 -0800

    Update logic of reaction wheel control algorithm

--- src/tasks/adcs/reaction_wheel_PD.py
+++ src/tasks/adcs/reaction_wheel_PD.py
@@ -7,34 +7,39 @@ import time
 KP = 1
 KD = 1
 ERROR_MARGIN = 0
-DELAY = 10
-
-prev_speed = 0
-current_speed = 0
+DELAY = 0.5
 
 
 def reaction_wheel_pd_control(desired_speed, motor):
-
-    # Retrieve the initial speed of the motor and start the initial timer
-    prev_speed = motor.get_speed()
-    prev_time = time.monotonic_ns()
-
     # Set the current speed to the initial speed of the motor
-    current_speed = prev_speed
+    current_speed = motor.get_speed()
+
+    # Initialize prev_error to be the current error
+    prev_error = desired_speed - current_speed
+
+    # Start the initial timer
+    prev_time = time.monotonic_ns()
 
     # While there is a significant error between the current and desired speed
     while abs(desired_speed - current_speed) > ERROR_MARGIN:
-
-        # Start the timer for the beginning of the PD loop and calculate P and D terms
+        # Calculate current error and get the current time
+        error = desired_speed - current_speed
         current_time = time.monotonic_ns()
-        p_term = current_speed * KP
-        d_term = KD * (current_speed - prev_speed) / (prev_time - current_time)
-        prev_speed = current_speed
 
-        # Reset the initial timer (end of PD loop) and apply changed values to motor speed
-        prev_time = time.monotonic_ns()
+        # Calculate terms to sum for motor speed
+        p_term = KP * error
+        dt = (current_time - prev_time) / 1e9  # Convert to seconds
+        d_term = KD * (error - prev_error) / dt
+
+        # Update motor speed
         motor.set_speed(p_term + d_term)
 
+        # Update prev_error to the current error
+        prev_error = error
+
         # Retrieve the new speed of the motor and implement loop delay
+        time.sleep(DELAY)
         current_speed = motor.get_speed()
-        time.sleep(10)
+
+        # Update prev_time to the time of finishing this iteration
+        prev_time = time.monotonic_ns()
