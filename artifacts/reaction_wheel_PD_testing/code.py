1df5fef2b5933cfafa76acb0e88cd3abc38fcbb4
SuperCoder109 <garagshashank7@gmail.com>
2026-03-11 19:18:11 -0700

    Implementing patch for unittest MagicMock issue

--- artifacts/reaction_wheel_PD_testing/code.py
+++ artifacts/reaction_wheel_PD_testing/code.py
@@ -2,11 +2,12 @@
 Testing code for reaction wheels using PD control loop.
 """
 
+from unittest.mock import patch
 import time
 import board
 from tasks.adcs.reaction_wheel_pd import reaction_wheel_pd_control as pd
 from drivers.reaction_wheel import ReactionWheel as motor
-from unittest.mock import MagicMock
+
 
 KP = 1
 KD = 1
@@ -14,30 +15,28 @@ ERROR_MARGIN = 0.1
 DELAY = 0.1
 DESIRED_VALUE = 50
 
-my_motor = motor(board.unsoll, board.diro, board.fg)
-if isinstance(my_motor.get_speed(), MagicMock):
-    my_motor.get_speed = MagicMock(return_value=0.0)
-
-prev_time = time.monotonic_ns()
-prev_error = 0
-current_speed = my_motor.get_speed()
-current_error = DESIRED_VALUE - current_speed
-
-# PD Control loop for motor speed
-while abs(current_error) > ERROR_MARGIN:
-
-    # Initialize motor speed and current error
+with patch("drivers.reaction_wheel.ReactionWheel.get_speed") as mocked_speed:
+    mocked_speed.return_value = 0.0
+    my_motor = motor(board.unsoll, board.diro, board.fg)
+    prev_time = time.monotonic_ns()
+    prev_error = 0
     current_speed = my_motor.get_speed()
     current_error = DESIRED_VALUE - current_speed
 
-    # Update parameters from PD loop
-    p_term, d_term, prev_time, prev_error = pd(
-        DESIRED_VALUE, current_speed, prev_error, prev_time
-    )
+    # PD Control loop for motor speed
+    while abs(current_error) > ERROR_MARGIN:
 
-    # Update motor speed to achieve desired speed
-    my_motor.set_speed(p_term + d_term)
-    time.sleep(DELAY)
+        # Initialize motor speed and current error
+        current_speed = my_motor.get_speed()
+        current_error = DESIRED_VALUE - current_speed
 
+        # Update parameters from PD loop
+        p_term, d_term, prev_time, prev_error = pd(
+            DESIRED_VALUE, current_speed, prev_error, prev_time
+        )
 
-my_motor.set_speed(0)
+        # Update motor speed to achieve desired speed
+        my_motor.set_speed(p_term + d_term)
+        time.sleep(DELAY)
+
+    my_motor.set_speed(0)
--- artifacts/rotation_PD/code.py
+++ artifacts/rotation_PD/code.py
@@ -2,47 +2,47 @@
 Program to rotate satellite by a certain value (degrees).
 """
 
+from unittest.mock import patch
 import time
 import board
 from tasks.adcs.reaction_wheel_pd import reaction_wheel_pd_control as pd
 from drivers.reaction_wheel import ReactionWheel as motor
-from unittest.mock import MagicMock
+
 
 ERROR_MARGIN = 0.1
 DELAY = 0.1
 SPEED = 50
 DESIRED_ANGLE = 90
 
-est_angle = 0.0
-my_motor = motor(board.unsoll, board.diro, board.fg)
-if isinstance(my_motor.get_speed(), MagicMock):
-    my_motor.get_speed = MagicMock(return_value=0.0)
-
-prev_time = time.monotonic_ns()
-prev_error = 0
-current_error = DESIRED_ANGLE - est_angle
-
-# PD Control loop for rotation
-while abs(current_error) > ERROR_MARGIN:
-    current_time = (
-        time.monotonic_ns()
-    )  # Initialize time for calculating motor spin time
-
-    # Calculate current angle
-    dt = (current_time - prev_time) / 1e9
-    current_speed = my_motor.get_real_speed() * 60  # Convert from rpm to rps
-    est_angle += current_speed * dt
-
+with patch("drivers.reaction_wheel.ReactionWheel.get_speed") as mocked_speed:
+    mocked_speed.return_value = 0.0
+    est_angle = 0.0
+    my_motor = motor(board.unsoll, board.diro, board.fg)
+    prev_time = time.monotonic_ns()
+    prev_error = 0
     current_error = DESIRED_ANGLE - est_angle
 
-    # Update parameters from PD loop
-    p_term, d_term, prev_time, prev_error = pd(
-        DESIRED_ANGLE, est_angle, prev_error, prev_time
-    )
+    # PD Control loop for rotation
+    while abs(current_error) > ERROR_MARGIN:
+        current_time = (
+            time.monotonic_ns()
+        )  # Initialize time for calculating motor spin time
 
-    # Update motor speed to achieve desired angle
-    my_motor.set_speed(p_term + d_term)
-    current_time = prev_time
-    time.sleep(DELAY)
+        # Calculate current angle
+        dt = (current_time - prev_time) / 1e9
+        current_speed = my_motor.get_real_speed() * 60  # Convert from rpm to rps
+        est_angle += current_speed * dt
 
-my_motor.set_speed(0)
+        current_error = DESIRED_ANGLE - est_angle
+
+        # Update parameters from PD loop
+        p_term, d_term, prev_time, prev_error = pd(
+            DESIRED_ANGLE, est_angle, prev_error, prev_time
+        )
+
+        # Update motor speed to achieve desired angle
+        my_motor.set_speed(p_term + d_term)
+        current_time = prev_time
+        time.sleep(DELAY)
+
+    my_motor.set_speed(0)
