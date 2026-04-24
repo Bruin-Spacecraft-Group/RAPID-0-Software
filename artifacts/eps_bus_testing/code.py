be6320bc66e08c2977b23dc8efa5084caf01e31d
AstroPancake72 <owen.t.rusk@gmail.com>
2026-04-24 16:11:21 -0700

    Verified enabling & disabling of buses

--- artifacts/eps_bus_testing/code.py
+++ artifacts/eps_bus_testing/code.py
@@ -6,6 +6,41 @@ from datastores import DsCommands
 import board
 import digitalio
 
+# Initialize pins
+
+en_3v3_bus_pin = digitalio.DigitalInOut(board.EN_3V3_BUS)
+en_3v3_bus_pin.direction = digitalio.Direction.OUTPUT
+
+en_5v_bus_pin = digitalio.DigitalInOut(board.EN_5V_BUS)
+en_5v_bus_pin.direction = digitalio.Direction.OUTPUT
+
+en_12vlp_bus_pin = digitalio.DigitalInOut(board.EN_12VLP_BUS)
+en_12vlp_bus_pin.direction = digitalio.Direction.OUTPUT
+
+en_12vhp_bus_pin = digitalio.DigitalInOut(board.EN_12VHP_BUS)
+en_12vhp_bus_pin.direction = digitalio.Direction.OUTPUT
+
+def set_3v3(eps_ds_commands: DsCommands, enable: bool):
+    en_3v3_bus_pin.value = enable
+    eps_ds_commands.bus_3v3_enabled = enable
+
+
+def set_5v(eps_ds_commands: DsCommands, enable: bool):
+    en_5v_bus_pin.value = enable
+    eps_ds_commands.bus_5v_enabled = enable
+
+
+
+def set_12vlp(eps_ds_commands: DsCommands, enable: bool):
+    en_12vlp_bus_pin.value = enable
+    eps_ds_commands.bus_12vlp_enabled = enable
+
+
+def set_12vhp(eps_ds_commands: DsCommands, enable: bool):
+    en_12vhp_bus_pin.value = enable
+    eps_ds_commands.bus_12vhp_enabled = enable
+
+
 if __name__ == "__main__":
     print("=" * 100)
     print(f'{"=" * 40}  EPS Bus Testing  {"=" * 41}')
@@ -16,12 +51,12 @@ if __name__ == "__main__":
     while True:
         print()
         command = input(
-            "Enter a command to control a bus (syntax: [enable/disable] [3v, 5v, 12vlp, 12vhp]):"
+            "Enter a command to control a bus (syntax: [enable/disable] [3v3, 5v, 12vlp, 12vhp]):"
         )
 
         split = command.split()
 
-        if split.__len__ != 2:
+        if len(split) != 2:
             print(
                 f"Invalid syntax. There should be 2 arguments separated by a space but you input {split.__len__}."
             )
@@ -49,34 +84,3 @@ if __name__ == "__main__":
             print(
                 f'Invalid syntax. The argument "{split[1]}" must be either "3v3", "5v", "12vlp", or "12vhp".'
             )
-
-
-
-
-def set_3v3(eps_ds_commands: DsCommands, enable: bool):
-    en_3v3_bus_pin = digitalio.DigitalInOut(board.EN_3V3_BUS)
-    en_3v3_bus_pin.direction = digitalio.Direction.OUTPUT
-    en_3v3_bus_pin.value = enable
-    eps_ds_commands.bus_3v3_enabled = enable
-
-
-def set_5v(eps_ds_commands: DsCommands, enable: bool):
-    en_5v_bus_pin = digitalio.DigitalInOut(board.EN_5V_BUS)
-    en_5v_bus_pin.direction = digitalio.Direction.OUTPUT
-    en_5v_bus_pin.value = enable
-    eps_ds_commands.bus_5v_enabled = enable
-
-
-
-def set_12vlp(eps_ds_commands: DsCommands, enable: bool):
-    en_12vlp_bus_pin = digitalio.DigitalInOut(board.EN_12VLP_BUS)
-    en_12vlp_bus_pin.direction = digitalio.Direction.OUTPUT
-    en_12vlp_bus_pin.value = enable
-    eps_ds_commands.bus_12vlp_enabled = enable
-
-
-def set_12vhp(eps_ds_commands: DsCommands, enable: bool):
-    en_12vhp_bus_pin = digitalio.DigitalInOut(board.EN_12VHP_BUS)
-    en_12vhp_bus_pin.direction = digitalio.Direction.OUTPUT
-    en_12vhp_bus_pin.value = enable
-    eps_ds_commands.bus_12vhp_enabled = enable
