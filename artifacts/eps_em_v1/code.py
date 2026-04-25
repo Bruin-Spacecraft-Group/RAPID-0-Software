2fd0c8fb110212368eb3e42c4370ebe9e21511ae
AstroPancake72 <owen.t.rusk@gmail.com>
2026-04-24 20:02:55 -0700

    updating bus enable name change and commenting out eps_em_v1 artifact

--- artifacts/eps_em_v1/code.py
+++ artifacts/eps_em_v1/code.py
@@ -7,6 +7,8 @@ import asyncio
 import bms
 import icd
 import monitoring
+import board
+import digitalio
 
 import datastore as ds
 
@@ -32,31 +34,28 @@ if __name__ == "__main__":
     # TODO: uncomment to run flight code (only once the electrical parts of the PCB are validated)
     # asyncio.run(gathered_task())
 
-    # import board
-    import digitalio
-
     # TODO: uncomment pins to run electrical tests and validate the PCB
-    for BMS_SHD in [
-        # board.S1_CHARGE_SHD,
-        # board.S1_DISCHARGE_SHD,
-        # board.S2_CHARGE_SHD,
-        # board.S2_DISCHARGE_SHD,
-        # board.S3_CHARGE_SHD,
-        # board.S3_DISCHARGE_SHD,
-    ]:
-        pin = digitalio.DigitalInOut(BMS_SHD)
-        pin.direction = digitalio.Direction.OUTPUT
-        pin.value = False
-    for EN_PSU in [
-        # board.EN_3V3_BUS,
-        # board.EN_5V_BUS,
-        # board.EN_12VLP_BUS,
-        # board.EN_12VHP_BUS,
-        # board.MPPT_EN
-    ]:
-        pin = digitalio.DigitalInOut(EN_PSU)
-        pin.direction = digitalio.Direction.OUTPUT
-        pin.value = True
-        # nda_libraries.run_if_nda_libraries_available(run_secret_functions)
-    while True:
-        pass
+    # for BMS_SHD in [
+    #     # board.S1_CHARGE_SHD,
+    #     # board.S1_DISCHARGE_SHD,
+    #     # board.S2_CHARGE_SHD,
+    #     # board.S2_DISCHARGE_SHD,
+    #     # board.S3_CHARGE_SHD,
+    #     # board.S3_DISCHARGE_SHD,
+    # ]:
+    #     pin = digitalio.DigitalInOut(BMS_SHD)
+    #     pin.direction = digitalio.Direction.OUTPUT
+    #     pin.value = False
+    # for EN_PSU in [
+    #     # board.EN_3V3_BUS,
+    #     # board.EN_5V_BUS,
+    #     # board.EN_12VLP_BUS,
+    #     # board.EN_12VHP_BUS,
+    #     # board.MPPT_EN
+    # ]:
+    #     pin = digitalio.DigitalInOut(EN_PSU)
+    #     pin.direction = digitalio.Direction.OUTPUT
+    #     pin.value = True
+    #     # nda_libraries.run_if_nda_libraries_available(run_secret_functions)
+    # while True:
+    #     pass
