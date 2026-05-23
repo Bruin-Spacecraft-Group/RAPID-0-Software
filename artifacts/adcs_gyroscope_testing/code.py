9b1a3cecfb82ee16fd87931bb9708fe87a404117
TinFoiKa <aaronli69@outlook.com>
2026-05-22 17:03:47 -0700

    reaction_wheel testing

--- artifacts/adcs_gyroscope_testing/code.py
+++ artifacts/adcs_gyroscope_testing/code.py
@@ -2,7 +2,29 @@ from drivers import bmi088
 import asyncio
 import busio
 import microcontroller
+from drivers import reaction_wheel
 
+spi = busio.SPI(
+        microcontroller.pin.PA05,
+        MOSI=microcontroller.pin.PA07,
+        MISO=microcontroller.pin.PA06,
+    )
+
+gyro = bmi088.Bmi088Gyro(
+    spi,
+    cs_gyro_pin_or_dio=microcontroller.pin.PE10,  # CS1
+    baudrate=1600,
+    polarity=0,
+    phase=0,
+    read_dummy_bytes=0,
+    cs_active_low=True,
+)
+
+rw = reaction_wheel.ReactionWheel(
+    microcontroller.pin.PA00,
+    microcontroller.pin.PA01,
+    microcontroller.pin.PA04
+)
 
 def _to_int16(msb, lsb):
     value = (msb << 8) | lsb
@@ -10,24 +32,7 @@ def _to_int16(msb, lsb):
         value -= 0x10000
     return value
 
-
 async def run_fixed_read():
-    spi = busio.SPI(
-        microcontroller.pin.PA05,
-        MOSI=microcontroller.pin.PA07,
-        MISO=microcontroller.pin.PA06,
-    )
-
-    gyro = bmi088.Bmi088Gyro(
-        spi,
-        cs_gyro_pin_or_dio=microcontroller.pin.PE10,  # CS1
-        baudrate=1600,
-        polarity=0,
-        phase=0,
-        read_dummy_bytes=0,
-        cs_active_low=True,
-    )
-
     await gyro.begin(verify_chip_id=False)
     print("fixed: CS1 mode=(0,0) dummy=0 baud=1600 cs_active_low=True")
 
@@ -63,7 +68,14 @@ async def run_fixed_read():
 
     print("DONE")
 
+# reaction wheel code
+async def spin_input_speed():
+    while True:
+        pc = input("Speed 0-100: ")
+        dc = pc/100 * (2**16-1)
+        rw.set_speed(dc)
 
 if __name__ == "__main__":
     print("is running...")
     asyncio.run(run_fixed_read())
+    asyncio.run(spin_input_speed())
