218a1d70431ef4cac4db43ede36a4afe9e47730d
AstroPancake72 <owen.t.rusk@gmail.com>
2026-05-29 16:49:17 -0700

    Remove old applications folder (was moved to artifacts before)

--- applications/cdh_analogio_test/code.py
+++
@@ -1,8 +0,0 @@
-import analogio
-import microcontroller
-import board
-
-if __name__ == "__main__":
-    something = analogio.AnalogIn(microcontroller.pin.PA04)
-    print(something.value)
-    pass
